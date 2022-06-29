fc723f30fb25b053561d4f3fa82f9f5a319f4398
«Aleksandr» <navalniu202@gmail.com>
2022-06-29 16:12:25 +0300

    HomeWork002

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,17 @@
+﻿void cube(int x)
+{
+          for (int i = 1; i <= x; i++)
+          {
+                    Console.Write((i*i)*i + " ");
+          }
+}
+
+
+
+
+
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+cube(x);
