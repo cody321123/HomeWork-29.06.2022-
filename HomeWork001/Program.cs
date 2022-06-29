1c3adf082b00ecde8fdf4a952e49fa54897632e5
«Aleksandr» <navalniu202@gmail.com>
2022-06-29 16:00:19 +0300

    HomeWork001

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,19 @@
+﻿void checkNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+       Console.WriteLine("Да"); 
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
+    
+}
+
+Console.WriteLine("Введите 5 значное число");
+
+string number = Console.ReadLine();
+
+checkNumber(number);
